64e46389bdc24464ef0a4b77acc499d519842ef6
rustem <rustem@icoosoft.com>
2021-06-21 23:37:20 +0500

    feat: done with 1-3 tasks

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,28 @@
 package subtask1
 
+import java.lang.Exception
+import java.time.*
+
 class DateFormatter {
 
     // TODO: Complete the following function
     fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
+        return try {
+            val date = ZonedDateTime.of(year.toInt(), month.toInt(), day.toInt(), 0, 0, 0, 0, ZoneId.of("UTC"))
+            val arrayOfMonths = arrayOf("января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря")
+            val dayOfTheWeek = when (date.dayOfWeek) {
+                DayOfWeek.MONDAY -> "понедельник"
+                DayOfWeek.TUESDAY -> "вторник"
+                DayOfWeek.WEDNESDAY -> "среда"
+                DayOfWeek.THURSDAY -> "четверг"
+                DayOfWeek.FRIDAY -> "пятница"
+                DayOfWeek.SATURDAY -> "суббота"
+                DayOfWeek.SUNDAY -> "воскресенье"
+            }
+            val month = arrayOfMonths[month.toInt() - 1]
+            "$day $month, $dayOfTheWeek"
+        } catch (e: Exception) {
+            "Такого дня не существует"
+        }
     }
 }
--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -4,6 +4,16 @@ class Abbreviation {
 
     // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        val string = StringBuilder()
+        var ai = 0
+        var bi = 0
+        while (ai < a.length && bi < b.length) {
+            if (a[ai].toUpperCase() == b[bi]) {
+                string.append(a[ai].toUpperCase())
+                bi++
+            }
+            ai++
+        }
+        return if (string.toString() == b) "YES" else "NO"
     }
 }
